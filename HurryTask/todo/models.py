f7faa2be30778324dbbdd88aa1a3272cff737bd3
IsaiasTolP <toledop.isaias@gmail.com>
2024-10-14 19:55:00 +0100

    Cambio al modelo

--- HurryTask/todo/models.py
+++ HurryTask/todo/models.py
@@ -5,7 +5,7 @@ class Task(models.Model):
     name = models.CharField(max_length=50)
     content = models.TextField()
     done = models.BooleanField(default=False)
-    slug = models.SlugField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
     date_limit = models.DateTimeField()
 
     def __str__(self) -> str:
