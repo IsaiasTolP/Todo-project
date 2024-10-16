66fe4100634a4769e5966d6fffbd940ddc8bfc3b
IsaiasTolP <toledop.isaias@gmail.com>
2024-10-16 18:55:27 +0100

    Primera migración

--- supertodo/tasks/models.py
+++ supertodo/tasks/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Task(models.Model):
     name = models.CharField(max_length=50)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     slug = models.SlugField(unique=True)
     done = models.BooleanField(default=False)
-    complete_before = models.DateTimeField()
+    complete_before = models.DateTimeField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
